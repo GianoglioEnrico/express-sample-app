d818f2079fae4c40701fe25b301464964817c78f
GianoglioEnrico <enrico.gianoglio@ibm.com>
2021-06-25 09:20:03 +0200

    WebHook and GitOps

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,7 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Enrico\'s Express with WebHook 3' });
+  res.render('index', { title: 'Enrico\'s Express with WebHook and GitOps' });
 });
 
 module.exports = router;
