2cf4dc1109915ae5ecc11f847784c7c60c7e2087
GianoglioEnrico <enrico.gianoglio@ibm.com>
2021-06-25 09:26:57 +0200

    GitOps Pipeline

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,7 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Enrico\'s Express with WebHook and GitOps' });
+  res.render('index', { title: 'Enrico\'s Express with WebHook and GitOps Pipeline' });
 });
 
 module.exports = router;
